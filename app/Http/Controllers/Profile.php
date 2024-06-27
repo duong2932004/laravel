a1f69373b087bdcf852ff925d609fe2dc81c13b5
Nguyễn Thế Dương <duong2932004@gmail.com>
2024-06-27 22:38:58 +0700

    lab1

---
+++ app/Http/Controllers/Profile.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class Profile extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = [
+            'name' => "Nguyễn Thế Dương",
+            'age' => 20,
+        ];
+        return view('profile', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(cr $cr)
+    {
+        //
+    }
+}
